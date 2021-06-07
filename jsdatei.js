02ff9ffeb2aba22f982f4e11bb051ed1e3d81272
Kevin Bertolini <76094099+kevin9xx@users.noreply.github.com>
2021-06-07 21:39:23 +0200

    Add files via upload
    Website Projekt

---
+++ jsdatei.js
@@ -0,0 +1,35 @@
+function bosnien() {
+  alert("Sie wollen Bosnien besser erkundigen? Kein Problem:)");
+  window.open("uebersicht.html#bosnien");
+  window.close("index.html");
+}
+
+function kosovo() {
+  alert("Sie wollen den Kosovo besser erkundigen? Kein Problem:)");
+  window.open("uebersicht.html#kosovo");
+  window.close("index.html");
+}
+
+function serbien() {
+  alert("Sie wollen Serbien besser erkundigen? Kein Problem:)");
+  window.open("uebersicht.html#serbien");
+  window.close("index.html");
+}
+
+function montenegro() {
+  alert("Sie wollen Montenegro besser erkundigen? Kein Problem:)");
+  window.open("uebersicht.html#montenegro");
+  window.close("index.html");
+}
+
+function mazedonien() {
+  alert("Sie wollen Mazedonien besser erkundigen? Kein Problem:)");
+  window.open("uebersicht.html#mazedonien");
+  window.close("index.html");
+}
+
+function albanien() {
+  alert("Sie wollen Albanien besser erkundigen? Kein Problem:)");
+  window.open("uebersicht.html#albanien");
+  window.close("index.html");
+}
